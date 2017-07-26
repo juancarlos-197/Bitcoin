c9bd788129fd6f4c0c1b2a9cd5b1f56c8c3fd339
Juan Carlos Alban luna <juancarlos-197@unimayor.edu.co>
2017-07-26 10:00:46 -0500

    Creacion de Tabs

---
+++ app/src/main/java/com/example/juanc/bitcoin/Tab2Categoria.java
@@ -0,0 +1,20 @@
+package com.example.juanc.bitcoin;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by juanc on 26/7/2017.
+ */
+
+public class Tab2Categoria extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2categoria_inicio, container, false);
+
+        return rootView;
+    }
+}
